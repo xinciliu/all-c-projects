87cced9f8cc0e8c0be893a373ffd33d028b05206
xinciliu <58103065+xinciliu@users.noreply.github.com>
2020-08-14 11:05:41 -0400

    Update main (25).cpp

--- main (25).cpp
+++ main (25).cpp
@@ -1,9 +1,4 @@
-//cs2024 
-//Assignment 9 
-//Xinci Liu
-//xl597
-//11/05/2019
-//using c++11
+
 
 #include <iostream>
 
@@ -12,10 +7,7 @@
 #include "MenuCommandH.h"
 #include "BankH.h"
 
-//
-// Our main() is pretty simple for this assignment.   Just set up the menus and call
-// select() on the top level menu
-//
+
 
 int main(int argc, char *argv[])
 {
@@ -41,20 +33,9 @@ int main(int argc, char *argv[])
 
   Menu mainMenu(mainMenuItems,4,'0',"Main");
 
-  // Kick off the whole process by just calling select() on the top level menu
-  bool keepGoing = mainMenu.select();
+\  bool keepGoing = mainMenu.select();
 
-  // What about deleting our dynamically allocated pointers?   
-  //
-  // This is a tough one.   It's not really the responsibility of the Menu class to delete
-  // those pointers because it wasn't the one who allocated them in the first place.   We
-  // could have set up multiple local variables for all the MenuCommand instances (as stack
-  // variables instead of being dynamically allocated) and then pass the address of those 
-  // variables to the respective Menu constructors.   Then no "freeing" would be necessary.
-  // Keep in mind that only works if the local variables are in a scope that stays active for
-  // the duration of the application which is usually only the case in main().   For now we
-  // won't worry about freeing the pointers as they need to stay allocated until the 
-  // application terminates anyway...
+ 
 
   std::cout << "Done" << std::endl;
 }
